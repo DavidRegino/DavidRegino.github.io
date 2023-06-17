8aed41aa7971f16dd2ef4380b2119c6bfa7270cb
DavidRegino <124410591+DavidRegino@users.noreply.github.com>
2023-06-16 19:19:04 -0700

    Add files via upload

--- parcial3/practica7.php
+++ parcial3/practica7.php
@@ -4,60 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 7</title>
     <link rel="stylesheet" href="css/bootstrap.css">
-    <title>Document</title>
     <style>
-        #dado {
-            width: 100px; /* Establecer el ancho deseado */
-            height: 100px; /* Establecer la altura deseada */
+        .casilla {
+            height: 80px;
+            font-weight: bold;
+        }
+        .escalera {
+            background-image: url('escalera.png');
+            background-size: cover;
+        }
+        .serpiente {
+            background-image: url('serpiente.png');
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1>Serpientes y escaleras</h1>
-        <?php
-
-// Colores
-$colores = array('#C80606', '#FFA200', '#4BEF4E', '#4BEFED', '#3535FF');
-
-// Dibujar el tablero
-echo '<table style="border-collapse: collapse;">';
-$numero = 100;
-for ($i = 0; $i < 10; $i++) {
-    echo '<tr>';
-    for ($j = 0; $j < 10; $j++) {
-        $color = $colores[array_rand($colores)];
-        echo '<td style="width: 60px; height: 60px; text-align: center; background-color: ' . $color . ';">' . $numero . '</td>';
-        $numero--;
-    }
-    echo '</tr>';
-}
-echo '</table>';
-?>
-    
-
-    <?php
-    //randomizer
-    function dado(){
-        $dado = array('dado/1.png','dado/2.png','dado/3.png','dado/4.png','dado/5.jpg','dado/6.jpg');
-        $numero = $dado [rand(0, count($dado)-1)];
-        return $numero;
-    }
-    //seleccion
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['boton'])) {
-        }
-    } else {
-        $numero = 'dado/1.png'; // Imagen por defecto al cargar la página
-    }
-    $numero=dado();
+    <?php 
+       $colores = ["#C80606", "#FFA200", "#4BEF4E", "#4BEFED", "#3535FF"];
     ?>
-    
-    <form method="POST">
-        <input type="submit" value="Dado" name="dado">
-    </form>
-    <img id="dado" src="<?php echo $numero?>" alt="numero">
+    <div class="container">
+        <h1>Práctica 7 - Tablero de serpientes y escaleras</h1><hr>
+        <form action="">
+            <div class="row">
+                    <?php
+                        for($i=100; $i>0; $i--){
+                            if($i == 4 || $i == 9 || $i == 33 || $i == 28 || $i == 40 || $i == 80 ||$i == 71 ||$i == 77){
+                                echo "<div class='col-1 card m-1 casilla escalera' style='background-color:".$colores[rand(0,4)].";'>".$i."</div>";
+                            } else if($i == 17 || $i == 54 || $i == 58 || $i == 64 || $i == 87 || $i == 93 ||$i == 95 ||$i == 99 ||$i == 36){
+                                echo "<div class='col-1 card m-1 casilla serpiente' style='background-color:".$colores[rand(0,4)].";'>".$i."</div>";
+                            } else {
+                                echo "<div class='col-1 card m-1 casilla' style='background-color:".$colores[rand(0,4)].";'>".$i."</div>";
+                            }
+                        }
+                    ?>
+            </div>
+        </form>
     </div>
+
 </body>
 </html>
+
